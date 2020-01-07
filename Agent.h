7f8c1eb02ea5df79b8d8c67d8ab59d7b80b4d9a3
Clément Guilbert <clement.guilbert@etu.udamail.fr>
2020-01-07 08:59:52 +0100

    ça fait ching et ça fait vroum

--- Agent.h
+++ Agent.h
@@ -1,3 +1,11 @@
 class Agent{
-
+        int force;
+        int dureeDeVie;
+    public:
+        void virtual live();
+        Agent(int f, int dDV);
+        int getForce();
+        int getDureeDeVie();
+        void setForce();
+        void setDureeDeVie;
 };
--- Survivant.h
+++ Survivant.h
@@ -1,3 +1,16 @@
+#include "Zombie.h"
+
+class Zombie;
+
 class Survivant : public Agent{
-    
+    float tauxRepro;
+    int killCount;
+    public:
+    void Attaquer(Zombie& z);
+    void live();
+    Survivant(int tR, int kC);
+    float getTauxRepro();
+    int getKillCount();
+    void setTauxRepro(float tR);
+    void setKillCount(int kC);
 };
