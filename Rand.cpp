9d545029c950464dcdb3e9bf61f3e3b2043bdb79
François POULY <francois.pouly@etu.uca.fr>
2020-01-27 12:31:44 +0100

    Ajout du .gitignore et ça compile

--- Rand.cpp
+++ Rand.cpp
@@ -1,16 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-
-/* Period parameters */  
-#define N 624
-#define M 397
-#define MATRIX_A 0x9908b0dfUL   /* constant vector a */
-#define UPPER_MASK 0x80000000UL /* most significant w-r bits */
-#define LOWER_MASK 0x7fffffffUL /* least significant r bits */
-
-static unsigned long mt[N]; /* the array for the state vector  */
-static int mti=N+1; /* mti==N+1 means mt[N] is not initialized */
+#include "Rand.hpp"
 
 /* initializes mt[N] with a seed */
 void init_genrand(unsigned long s)
